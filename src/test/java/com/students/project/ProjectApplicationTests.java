84fd55e24990cfe56b49be299e6153817de39124
Kommit <aksaghovafatima@gmail.com>
2023-10-11 14:51:20 +0300

    Commit

---
+++ src/test/java/com/students/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.students.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
