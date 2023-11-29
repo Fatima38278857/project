6e399e8e4ad347bb7bed1d81f4a557f7ffac6c9b
Kommit <aksaghovafatima@gmail.com>
2023-11-29 15:16:17 +0300

    конфигурационный файл с ОШИБКОЙ

---
+++ src/main/java/com/students/project/controller/InfoController.java
@@ -0,0 +1,17 @@
+package com.students.project.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/info")
+public class InfoController {
+
+    @Value("${server.port}")
+   private String port;
+
+    public String getPort(){
+        return port;
+    }
+}
