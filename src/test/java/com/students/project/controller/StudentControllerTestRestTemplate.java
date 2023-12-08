6280ee2efef348e41ab12846014563a7e32231f4
Kommit <aksaghovafatima@gmail.com>
2023-12-08 21:24:51 +0300

    Тесты с ошибкой

--- src/test/java/com/students/project/controller/StudentControllerTestRestTemplate.java
+++ src/test/java/com/students/project/controller/StudentControllerTestRestTemplate.java
@@ -1,14 +1,19 @@
 package com.students.project.controller;
 
 import com.students.project.model.Student;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class StudentControllerTestRestTemplate {
@@ -16,55 +21,83 @@ class StudentControllerTestRestTemplate {
 
     @LocalServerPort
     private int port;
-
-    @Autowired
-    private StudentController studentController;
-
     @Autowired
     private TestRestTemplate restTemplate;
 
 
-    @Test
-    void contextLoads() throws Exception {
-        Assertions.assertThat(studentController).isNotNull();
-    }
+    Student student1 = new Student(1L, "Bася", 22);
+    Student student2 = new Student(2L, "vdmcm", 25);
+    Student student3 = new Student(3L, "dcdcsdcs", 22);
+    Student student4 = new Student(4L, "vdvdkv", 22);
+    Student student5 = new Student(5L, "vdvdkcndnvv", 22);
+    Student posted1 = restTemplate.postForObject("http://localhost:" + port + "/student", student1, Student.class);
+    Student posted2 = restTemplate.postForObject("http://localhost:" + port + "/student", student2, Student.class);
+    Student posted3 = restTemplate.postForObject("http://localhost:" + port + "/student", student3, Student.class);
+    Student posted4 =  restTemplate.postForObject("http://localhost:" + port + "/student", student4, Student.class);
+    Student posted5  = restTemplate.postForObject("http://localhost:" + port + "/student", student5, Student.class);
 
     @Test
     void get() {
-        Student student1 = new Student(1L,"Bася", 22);
-        Student postedStudent = this.restTemplate.postForObject("http://localhost:" + port + "/student",   student1, Student.class);
-        Student actualStudent = this.restTemplate.getForObject("http://localhost:" + port + "/student" + "?id=" + postedStudent.getId(), Student.class);
-        assertEquals(postedStudent, actualStudent);
-
+        Student actualStudent = this.restTemplate.getForObject("http://localhost:" + port + "/student/" + posted1.getId(), Student.class);
+        assertEquals(student1, actualStudent);
     }
 
     @Test
     void post_success() {
-        Student student1 = new Student(1L,"Bася", 22);
-        Assertions
-                .assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/student",   student1, Student.class))
-                .isNotNull();
+        assertEquals(student1, restTemplate.postForObject("http://localhost:" + port + "/student", student1, Student.class));
     }
 
     @Test
     void put() {
-    }
+        HttpEntity<Student> entity = new HttpEntity<Student>(student3);
+        Student response = restTemplate.exchange("http://localhost:" + port + "/student", HttpMethod.PUT, entity, Student.class).getBody();
 
-    @Test
-    void ydolit() {
-    }
+        assertEquals(student3, response);
 
-    @Test
-    void findStudents() {
     }
 
 
     @Test
-    void filtrAge() {
+    void deleteStudent() {
+//        Student postedStudent = this.restTemplate.postForObject("http://localhost:" + port + "/student", student1, Student.class);
+        String url = "http://localhost:" + port + "/student/" + posted4.getId();
+        restTemplate.delete(url);
+        Student actualStudent = restTemplate.getForObject("http://localhost:" + port + "/student/" + student4.getId(), Student.class);
+        assertNull(actualStudent.getId());
+
     }
 
     @Test
-    void geyAllStudent() {
+    void findStudents()  {
+//        Student student1 = new Student(1L, "Bася", 22);
+//        Student student2 = new Student(2L, "Bася", 25);
+//        Student student3 = new Student(3L, "Bася", 22);
+        List<Student> expectedStudent = new ArrayList<>(List.of(
+                student1,
+                student3
+        ));
+//        restTemplate.postForObject("http://localhost:" + port + "/student", student1, Student.class);
+//        restTemplate.postForObject("http://localhost:" + port + "/student", student2, Student.class);
+//        restTemplate.postForObject("http://localhost:" + port + "/student", student3, Student.class);
+        String url = "http://localhost:" + port + "/student" + "?age=" + "22";
+        List<?> actualStudent = restTemplate.getForObject(url, List.class);
+        assertEquals(expectedStudent, actualStudent);
+
+
+    }
+
+    @Test
+    void filterAge() {
+        List<Student> expectedStudent = new ArrayList<>(List.of(
+                student1,
+                student3
+        ));
+//        restTemplate.postForObject("http://localhost:" + port + "/student", student1, Student.class);
+//        restTemplate.postForObject("http://localhost:" + port + "/student", student2, Student.class);
+//        restTemplate.postForObject("http://localhost:" + port + "/student", student3, Student.class);
+        String url = "http://localhost:" + port + "/student" + "/min-And-max" + "?min=" + "14" + "&max=" + "17";
+        List<?> actualStudent = restTemplate.getForObject(url, List.class);
+        assertEquals(expectedStudent, actualStudent);
     }
 
     @Test
